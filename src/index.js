c26975dc9fa1c26be4ad0da18c1083cf9461365b
anahitavarji <anahitavarji@gmail.com>
2021-02-10 00:12:23 -0500

    first try of connecting mySQL

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 
 const api = express();
+api.use(express.static(__dirname + '/public'));
+//creates directory for folder rather using './...'
+api.use(bodyParser.json());
+//JSON is going to parse the data you get and add it to the request body parameter
+
 api.listen(3000, () => {
 console.log('API up and running!')
 });
 
-api.get('/', (req, res) => {
-console.log(req);
-res.send('Hello, world!');
+
+api.post('/add', (req, res) => {
+    console.log(req.body);
+    res.send('it works!');
 });
+//post request
+//send request when we add a new item
+//first add router string 
+//then callback function
+//first post route
+
+
